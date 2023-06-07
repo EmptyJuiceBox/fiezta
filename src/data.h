da5681229a5a29e6ede8de050c366d4c37971ef5
Stef Velzel <stefvelzel@hotmail.com>
2023-06-07 21:45:36 +0200

    data.h cleanup

--- src/data.h
+++ src/data.h
@@ -11,7 +11,7 @@ public:
 	~FrameData();
 
 	size_t count() { return gfx_group_get_num_bindings(group); }
-	size_t size() { auto bind = gfx_group_get_binding(group, 0); return bind.elementSize; }
+	size_t size() { return gfx_group_get_binding(group, 0).elementSize; }
 	GFXMemoryFlags flags() { return group->flags; }
 	GFXBufferUsage usage() { return group->usage; }
 
